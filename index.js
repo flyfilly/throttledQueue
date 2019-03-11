1ca0b4d57f8149ca7428e1d2dc932d8894392fec
Felipe Sanchez <fnsanchez317@gmail.com>
2019-03-11 16:06:53 -0500

    making a problem

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const events = require("events");
 
-const ThrottledQueue = function (throttle = 1000) {
+const ThrottledQueue = function(throttle = 1000) {
   this.queue = [];
   this.interval = null;
   this.throttle = throttle;
@@ -8,47 +8,47 @@ const ThrottledQueue = function (throttle = 1000) {
 
 ThrottledQueue.prototype = new events.EventEmitter();
 
-ThrottledQueue.prototype.add = function (item) {
-  this.queue.unshift(item);
-  this.emit("addedItem", item);
+ThrottledQueue.prototype.add = function(thingy) {
+  this.queue.unshift(thingy);
+  this.emit("addedItem", thingy);
 };
 
-ThrottledQueue.prototype.take = function () {
+ThrottledQueue.prototype.take = function() {
   const item = this.queue.pop();
-  this.emit('removedItem', item);
+  this.emit("removedItem", item);
 
   if (0 === this.queue.length) {
-    this.emit('noItems');
+    this.emit("noItems");
     this.stop();
   }
 };
 
-ThrottledQueue.prototype.process = function () {
+ThrottledQueue.prototype.process = function() {
   if (!this.interval) {
-    return this.interval = setInterval(async () => {
-      this.emit('itemToProcess', this.take());
-    }, this.throttle);
+    return (this.interval = setInterval(async () => {
+      this.emit("itemToProcess", this.take());
+    }, this.throttle));
   }
 
   return this.interval;
 };
 
-ThrottledQueue.prototype.start = function () {
+ThrottledQueue.prototype.start = function() {
   if (this.interval) {
     return "The queue is already running.";
   }
 
   this.process();
   return "the queue is now running.";
-}
+};
 
-ThrottledQueue.prototype.stop = function () {
+ThrottledQueue.prototype.stop = function() {
   if (!this.interval) {
     return "The queue is not currrently running.";
   }
   clearInterval(this.interval);
   this.interval = null;
   return "queue stopped.";
-}
+};
 
 module.exports = ThrottledQueue;
